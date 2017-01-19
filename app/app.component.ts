82d1d1864082bd74dc244c1e5fc3e2cdfb57519c
Edward Pantoja <edward.pantoja@sjsu.edu>
2017-01-18 19:13:07 -0800

    Initial Commit

---
+++ app/app.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+import { GithubService } from './services/github.services';
+@Component({
+  selector: 'my-app',
+  template: `<profile></profile>`,
+  providers: [GithubService]
+})
+export class AppComponent  { name = 'Angular'; }
---
+++ app/services/github.services.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers } from "@angular/http";
+import 'rxjs/add/operator/map';
+
+@Injectable()
+
+export class GithubService{
+  private username:string;
+  private client_id = '4c92807a2aa30bd5a331';
+  private client_secret ='31daefcbbd2ebba461543591181454134d400154';
+
+  constructor(private _http: Http){
+    console.log('Github Service Ready...');
+    this.username = 'eddiepantoja';
+  }
+
+  getUser(){
+    return this._http.get('http://api.github.com/users/' + this.username + '?client_id=' + this.client_id + '&client_secret' + this.client_secret)
+    .map(res => res.json());
+  }
+}
